530a1da08ed13e530a1c0378e418210593883057
Himynameislou <louie1598@gmail.com>
2020-03-04 21:55:02 +0000

    L 63 done. Creating Objects with Object.create

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,4 @@
-// Function Constructor
+/*// Function Constructor
 
 // var john = {
 //   name: 'John',
@@ -47,3 +47,27 @@ var lou = new Historian('Lou', 'Renaissance Architecture', 'Late 1600s');
 var lyndon = new Historian('Lyndon', 'JFK', 'Late 1960s');
 
 // console.log(sarah, lou, lyndon);
+*/
+
+// Object.create
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2016-this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+// The ABOVE method is not ideal because we have to add all the info line by line.  Object.create DOES let you add a second parameter.
+
+//so
+
+var jane = Object.create(personProto, {
+  name: {value:'Jane'},
+  yearOfBirth: {value:1969},
+  job: {value:'designer'},
+});
