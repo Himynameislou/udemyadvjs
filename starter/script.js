8def2f0736421e109c9a439c257537036f1ce237
Himynameislou <louie1598@gmail.com>
2020-03-04 22:59:17 +0000

    L 65 done. First Class Functions; Passing functions as arguments

--- starter/script.js
+++ starter/script.js
@@ -72,7 +72,6 @@ var jane = Object.create(personProto, {
   job: {value:'designer'},
 });
 
-*/
 
 // Primitives VS Objects
 
@@ -115,3 +114,48 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.name);
+*/
+
+
+//////////// Passing functions as arguments /////////////
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2016 - el;
+}
+
+// Creating another Callback fn to show how powerful this is
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+// Adding another to show the max heart rate that applies to an individuals age
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9-(0.67 * el));
+  } else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calculateAge);
+
+var fullAges = arrayCalc(ages, isFullAge);
+
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
